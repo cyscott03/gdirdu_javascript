d2a6800c1bffa64626cd0f9f16a0a2a80bb5d8d2
Cherrelle <cherrelle.scott@gmail.com>
2016-06-13 21:24:46 -0400

    first javascript code

---
+++ age_calculator.js
@@ -0,0 +1,7 @@
+alert('Welcome to the Age Calculator');
+var currentYear = 2016;
+var previousYear = 2015;
+var birthYear = prompt('What year were you born?');
+var possibleAgeOne = currentYear - birthYear;
+var possibleAgeTwo = previousYear - birthYear;
+window.alert('You are either ' + possibleAgeOne + ' or ' + possibleAgeTwo + ' years old.');
